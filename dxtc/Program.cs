35fad52c4b1d7a1ab16a1febd5eb22d1c1b7d70e
Jon Ander Hernandez <jonan.h@gmail.com>
2015-11-26 14:46:50 +0200

    Initial project commit

---
+++ dxtc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dxtc
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
